4f00a177cd4a0b87d892f282ab7a72595a0b52b8
Abdulaha Islam <abdulahaislam210917@gmail.com>
2021-12-21 23:01:57 +0600

    commit class 10

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -12,4 +12,10 @@ class PropertyController extends Controller
 
         return view( 'property.single', ['property' => $poperty]);
     }
+
+    public function index(Request $request) {
+        $latest_properties = Property::latest()->where('type', $request->type)->paginate(12);
+
+        return view('property.index', ['latest_properties' => $latest_properties]);
+    }
 }

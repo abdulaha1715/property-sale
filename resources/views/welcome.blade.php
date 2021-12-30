f6d511d2080b7ff76303d4258243488c137fecf5
Abdulaha Islam <abdulahaislam210917@gmail.com>
2021-12-30 17:14:13 +0600

    commit class 12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,54 +10,8 @@
     <!-- Search From Area -->
     <div class="-mt-10">
         <div class="container">
-            <div class="rounded-lg bg-white p-4 relative z-20 shadow-lg">
-                <form action="{{ route('properties') }}" method="GET" class="flex justify-between">
-                    <div class="flex w-7/12 justify-between items-center">
-                        <div class="flex flex-col mx-3">
-                            <select name="sale" class="border-0 focus:ring-0">
-                                <option value="">Buy or Rent</option>
-                                <option value="2">Buy</option>
-                                <option value="1">Rent</option>
-                            </select>
-                        </div>
-                        <div class="py-3 self-center border-gray-500 border"></div>
-                        <div class="flex flex-col mx-3">
-                            <select name="type" class="border-0 focus:ring-0">
-                                <option value="">Type</option>
-                                <option value="land">Land</option>
-                                <option value="apartment">Apartmet</option>
-                                <option value="villa">Villa</option>
-                            </select>
-                        </div>
-                        <div class="py-3 self-center border-gray-500 border"></div>
-                        <div class="flex flex-col mx-3">
-                            <select name="price" class="border-0 focus:ring-0">
-                                <option value="">Price</option>
-                                <option value="100000">0 - 100000</option>
-                                <option value="200000">100000 - 200000</option>
-                                <option value="300000">200000 - 300000</option>
-                                <option value="400000">300000 - 400000</option>
-                                <option value="500000">400000 - 500000</option>
-                                <option value="500001">500000<span class="font-serif">+</span></option>
-                            </select>
-                        </div>
-                        <div class="py-3 self-center border-gray-500 border"></div>
-                        <div class="flex flex-col mx-3">
-                            <select name="bedrooms" class="border-0 focus:ring-0">
-                                <option value="">Bedrooms</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="flex justify-between items-center w-5/12 ml-5">
-                        <input type="search" placeholder="Try to search for something" class="rounded-lg px-4 py-2 w-full mr-4 focus:border-gray-700 focus:ring-0">
-                        <button type="submit" class="btn bg-gray-800 text-white py-2 px-4 border-gray-800 border-2  rounded-md">Search</button>
-                    </div>
-                </form>
+            <div class="rounded-lg bg-white p-4 relative z-20 shadow-lg home-search">
+                @include('components.property-search-form')
             </div>
 
         </div>
